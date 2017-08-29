c2aaa386b458e86038b9678583f853a8710ad5bf
VeniGupta <venigupta1995@gmail.com>
2017-08-30 03:31:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+app.set("view engine", "pug");
+app.use("/", bodyParser.urlencoded({extended:false}) );
+app.use("/static", express.static(__dirname));
+app.use("/static", express.static(__dirname+"/assets"));
+
+app.post("/submit", function(req,res)
+{
+    //console.log(req.body);
+    var dob= Date.parse(req.body.dob);
+    var oneDay = 24*60*60*1000;
+    var currentDate = new Date();
+
+    var diffDays = Math.round(Math.abs((currentDate.getTime() - dob)/(oneDay)));
+    //console.log(diffDays);
+
+    res.render(
+        "index",
+        { message : req.body.firstname, days : diffDays}
+        // message: " Hey " + req.body.firstname +" you have lived on this planet "+ diffDays +" days"}
+
+    );
+});
+
+
+app.listen(3000);
